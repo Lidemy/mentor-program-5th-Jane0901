089d96c2faca9aa0e13ae1dba1e82ff4e177a2b4
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2021-05-03 15:14:09 +0800

    hw3

---
+++ homeworks/week3/hw3.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+function solve(lines) {
+  for (let i = 1; i < lines.length; i++) {
+    console.log(isPrime(lines[i]))
+  }
+}
+
+function isPrime(number) {
+  if (number === 1) return 'Composite'
+  const temp = []
+  for (let j = 1; j <= number; j++) {
+    if (number % j === 0) {
+      temp.push(j)
+    }
+  }
+  if (temp.length === 2) {
+    return 'Prime'
+  } else {
+    return 'Composite'
+  }
+}
