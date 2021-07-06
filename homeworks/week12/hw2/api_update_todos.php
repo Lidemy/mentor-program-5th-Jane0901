850a2596c98433fb63260bd8f2ae46c390582dfe
陳宜榛 <37102145+Jane0901@users.noreply.github.com>
2021-07-06 15:33:12 +0800

    hw2

---
+++ homeworks/week12/hw2/api_update_todos.php
@@ -0,0 +1,41 @@
+<?php
+  require_once('conn.php'); 
+  header('Content-type:application/json;charset=utf-8');
+  header('Access-Control-Allow-Origin: *');
+
+  if (empty($_POST['todo']) | empty($_POST['id'])) {
+    $json = array (
+      'ok' => false,
+      'message' => '資料不完整'
+    );
+    $response = json_encode($json);
+    echo $response;
+    die();
+  }
+
+  $id = $_POST['id'];
+  $todo = $_POST['todo'];
+  
+  $sql = 'UPDATE jane_todos SET todo = ? WHERE id = ?';
+  $stmt = $conn -> prepare($sql);
+  $stmt -> bind_param('si', $todo, $id);
+  $result = $stmt -> execute();
+
+  if (!$result) {
+    $json = array (
+      'ok' => false,
+      'message' => '更新失敗'
+    );
+    $response = json_encode($json);
+    echo $response;
+    die();
+  }
+
+  $json = array (
+    'ok' => false,
+    'message' => '更新成功！',
+    'id' => $id
+  );
+  $response = json_encode($json);
+  echo $response;
+?>
